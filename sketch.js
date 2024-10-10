616f520e19be49fd9cd9fc6a33c4187ae1077d63
Skyler Ledbetter <skeome@gmail.com>
2024-10-09 17:42:58 -0700

    Update

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let song;
+let amp;
+
+function preload() {
+    song=loadSound('Media/Audio/Journey to Another World.wav');
+}
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    amp = new p5.Amplitude();
+    song.play();
+}
+
+function draw(){
+    background(0);
+    let level = amp.getLevel();
+    let size = map(level, 0, 1, 0, width/2);
+    fill (255, 0, 0);
+    ellipse(width/2, height/2, size, size);
+}
