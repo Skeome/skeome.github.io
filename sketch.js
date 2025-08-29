7211af80a5e9e66dfaf5a535736fd94f09c81f7c
Skeome <skeome@gmail.com>
2025-08-29 02:27:16 -0700

    Modified sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,31 @@
 let song;
-let fft; let smoothing = 0.8; let bins = 512;
-let waveform = []; let r = 100;
+let fft;
+let smoothing = 0.8;
+let bins = 512;
+let waveform = [];
+let r = 100;
 
-function preload(){
-    song = loadSound("Media/Audio/Journey-to-Another-World.wav");
+function preload() {
+  song = loadSound("Media/Audio/Journey-to-Another-World.wav");
 }
 
-function setup(){
-    createCanvas(400, 400);
+function setup() {
+  createCanvas(400, 400);
+  fft = new p5.FFT(smoothing, bins);
+}
+
+function draw() {
+  background(220);
+  waveform = fft.waveform();
+
+  for (let i = 0; i < waveform.length; i++) {
+    let y = height / 2 + map(waveform[i], -1, 1, -r, r);
+    ellipse(i, y, 1, 1);
+  }
+}
+
+function mousePressed() {
+  if (!song.isPlaying()) {
     song.play();
-    fft = new p5.FFT(smoothing, bins);
-}
-
-function draw(){
-    background(220);
-    waveform = fft.waveform();
-    
-    for(let i=0; i<waveform.length; i++) {
-        let y = height/2 + map(waveform[i], -1, 1, -removeEventListener, r);
-        ellipse(i, y, 1, 1);
-    }
-
+  }
 }
